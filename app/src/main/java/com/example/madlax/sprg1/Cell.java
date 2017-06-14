b5819fdb3a1bf95225c569f6a12b05b3c3c37c91
hoshi-lab <aglioolio26@gmail.com>
2017-06-14 21:40:39 +0900

    最短経路条件の追加

--- app/src/main/java/com/example/madlax/sprg1/Cell.java
+++ app/src/main/java/com/example/madlax/sprg1/Cell.java
@@ -9,11 +9,15 @@ public class Cell {
     public boolean tansakuzumi;
     public boolean tansakuyotei;
     public int moveVariable;
+    public int rootX;
+    public int rootY;
     //コンストラクタ
     public Cell(){
         tansakuzumi = false;
         tansakuyotei= false;
         moveVariable = -5;
+        rootX = 0;
+        rootY = 0;
     }
     //メソッド
     public void writecell(int i,int j,Cell cell[][],Chapter chapter,Terrain[] terrain){
@@ -21,33 +25,42 @@ public class Cell {
         cell[i][j].tansakuyotei = false;
         cell[i][j].tansakuzumi = true;
         //周囲4マスの処理
-        if(i<chapter.getNumCellX()) {
-            if (cell[i+1][j].tansakuzumi==false) {
+        if(0<=i&&i<=chapter.getNumCellX()-1) {
+            if (cell[i+1][j].moveVariable > cell[i][j].moveVariable - terrain[chapter.field[i+1][j]].moveCost) {
                 cell[i+1][j].moveVariable = cell[i][j].moveVariable - terrain[chapter.field[i+1][j]].moveCost;
+                rootX = i;
+                rootY = j;
                 if (cell[i+1][j].moveVariable > 0) {
                     cell[i+1][j].tansakuyotei = true;
                 }
             }
         }
-        if(j<chapter.getNumCellY()){
-            if(cell[i][j+1].tansakuzumi==false)
-            {cell[i][j+1].moveVariable = cell[i][j].moveVariable -terrain[chapter.field[i][j+1]].moveCost;
-                if(cell[i][j+1].moveVariable >0){
-                    cell[i][j+1].tansakuyotei = true;
-                }
+        if(0<=j&&j<=chapter.getNumCellY()-1){
+            if(cell[i][j+1].moveVariable > cell[i][j].moveVariable -terrain[chapter.field[i][j+1]].moveCost) {
+                    cell[i][j + 1].moveVariable = cell[i][j].moveVariable - terrain[chapter.field[i][j + 1]].moveCost;
+                    rootX = i;
+                    rootY = j;
+                    if (cell[i][j + 1].moveVariable > 0) {
+                        cell[i][j + 1].tansakuyotei = true;
+                    }
+
             }
         }
-        if(i>0) {
-            if (cell[i-1][j].tansakuzumi==false) {
+        if(1<=i&&i<=chapter.getNumCellX()) {
+            if (cell[i-1][j].moveVariable > cell[i][j].moveVariable - terrain[chapter.field[i-1][j]].moveCost) {
                 cell[i-1][j].moveVariable = cell[i][j].moveVariable - terrain[chapter.field[i-1][j]].moveCost;
+                rootX = i;
+                rootY = j;
                 if (cell[i-1][j].moveVariable > 0) {
                     cell[i-1][j].tansakuyotei = true;
                 }
             }
         }
-        if(j>0) {
-            if (cell[i][j - 1].tansakuzumi==false) {
+        if(1<=j&&j<=chapter.getNumCellY()) {
+            if (cell[i][j - 1].moveVariable > cell[i][j].moveVariable - terrain[chapter.field[i][j-1]].moveCost) {
                 cell[i][j - 1].moveVariable = cell[i][j].moveVariable - terrain[chapter.field[i][j - 1]].moveCost;
+                rootX = i;
+                rootY = j;
                 if (cell[i][j - 1].moveVariable > 0) {
                     cell[i][j - 1].tansakuyotei = true;
                 }
